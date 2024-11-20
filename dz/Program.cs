91fbca21630357d3d9c7c7055310defab5ba7c7b
Надежда <@DESKTOP-EHSPKBR>
2024-11-20 23:11:06 +0200

    Добавьте файлы проекта.

---
+++ dz/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите значение х ");
+double x = 0;
+Console.Write("Введите количество членов ряда n ");
+int n = 0;
+Convert.ToInt32(Console.ReadLine());
+double sum = 0;
+for (int i = 0; i < n; i++)
+{
+    double numerator = Math.Cos(2 * i * x);
+    double denominator = (i+ 1) * (i+2);
+    sum += numerator / denominator;
+}
+    Console.WriteLine($"Сумма первых {n} членов ряда: {sum}");
+
